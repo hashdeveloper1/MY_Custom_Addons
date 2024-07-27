98f436d023b32f94f8614d64d9b54f07c44de9b7
Hashem Ahmed Mahmoud <hashemodoodeveloper@gmail.com>
2024-07-27 21:00:50 +0300

    add some dirictory to project security ststic views and some  modifid

--- my_custom_addons/todo_management/__manifest__.py
+++ my_custom_addons/todo_management/__manifest__.py
@@ -5,6 +5,9 @@
     'version': '16.0.0.1.0',
     'depends': ['base'],
     'data': [
+        'security/ir.model.access.csv',
+        'views/base_menu.xml',
+        'views/todo_task_view.xml',
     ],
     'application': True,
 }
